defae9e783a73f9cc300197f14df7721f2e26c6c
John Holliman <holliman@mit.edu>
2015-11-07 14:47:19 -0500

    Indent

--- speck.h
+++ speck.h
@@ -11,30 +11,30 @@ struct RoundResult {
 };
 
 class SpeckCipher {
-public:
-	SpeckCipher(uint64_t _key);
+	public:
+		SpeckCipher(uint64_t _key);
 
-	uint64_t encrypt(uint64_t plaintext);
-	uint64_t decrypt(uint64_t ciphertext);
-private:
-	RoundResult encrypt_round(uint64_t x, uint64_t y, uint64_t k);
-	RoundResult decrypt_round(uint64_t x, uint64_t y, uint64_t k);
-	void generate_key_schedule();
+		uint64_t encrypt(uint64_t plaintext);
+		uint64_t decrypt(uint64_t ciphertext);
+	private:
+		RoundResult encrypt_round(uint64_t x, uint64_t y, uint64_t k);
+		RoundResult decrypt_round(uint64_t x, uint64_t y, uint64_t k);
+		void generate_key_schedule();
 
-	uint64_t block_size;
-	uint64_t word_size;
+		uint64_t block_size;
+		uint64_t word_size;
 
-	uint64_t rounds;
+		uint64_t rounds;
 
-	uint64_t key;
-	uint64_t key_size;
-	std::vector<uint64_t> key_schedule;
+		uint64_t key;
+		uint64_t key_size;
+		std::vector<uint64_t> key_schedule;
 
-	uint64_t beta_shift;
-	uint64_t alpha_shift;
-	uint64_t mod_mask;
+		uint64_t beta_shift;
+		uint64_t alpha_shift;
+		uint64_t mod_mask;
 
-	std::vector<std::string> _valid_modes;
+		std::vector<std::string> _valid_modes;
 };
 
 #endif
